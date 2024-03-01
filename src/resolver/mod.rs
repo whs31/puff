4e58c15411021f72ed00c482491d0089dcec3c75
Dmitry Ryazancev <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-03-01 13:24:00 +0300

    pull start

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -2,6 +2,7 @@ pub mod dependency_tree;
 pub mod dependency;
 pub mod cache;
 pub mod push;
+pub mod pull;
 
 pub use dependency::Dependency;
 pub use dependency_tree::DependencyStack;
---
+++ src/resolver/pull.rs
@@ -0,0 +1,5 @@
+use crate::resolver::Dependency;
+
+pub fn pull_from_artifactory(dep: &Dependency) -> anyhow::Result<Vec<u8>> {
+
+}

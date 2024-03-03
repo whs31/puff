60e97a5186539ab1ff2770d0d3cb091224fb41c8
whs31 <75864292+whs31@users.noreply.github.com>
2024-03-03 04:22:32 +0300

    Update poppup.py

--- poppup.py
+++ poppup.py
@@ -2,6 +2,11 @@
 
 import requests
 import argparse
+import json
+import os
+import platform
+import shutil
+import tarfile
 
 default_credentials = {
     'user': 'gitlab_ci',
@@ -29,6 +34,8 @@ class Artifactory(object):
     def __init__(self, credentials):
         self.credentials = credentials
         self.url = 'http://213.170.107.251/artifactory/poppy-cxx-repo/radar/{name}/{name}-{version}-{arch}-{dist}.tar.gz'
+        self.api_url = 'http://213.170.107.251/artifactory/api/storage/poppy-cxx-repo/radar/{name}/{name}-{version}-{arch}-{dist}.tar.gz'
+        self.aql_url = 'http://213.170.107.251/artifactory/api/search/aql'
 
     def push(self, file, _name, _version, _arch, _dist):
         url = self.url.format(name=_name, version=_version, arch=_arch, dist=_dist)
@@ -36,6 +43,46 @@ class Artifactory(object):
         r = requests.put(url, data=file, auth=(self.credentials.user, self.credentials.token))
         print(f'response status code: {r.status_code}')
         return r
+
+    def install_latest(self, _arch):
+        query = 'items.find({"repo": "poppy-cxx-repo", "name": {"$match": "poppy-*"}}).sort({"$desc": ["created"]}).limit(2)'
+        r = requests.post(self.aql_url, data=query, auth=(self.credentials.user, self.credentials.token))
+        print(f'response status code: {r.status_code}')
+
+        jprint = lambda x: print(json.dumps(x, sort_keys=True, indent=4))
+        jprint(r.json())
+
+        latest = r.json()['results'][0]['name']
+        print(f'found latest: {latest}')
+        latest_url = self.url.format(name='poppy', version='', arch='', dist='')
+        latest_url = latest_url[:latest_url.rfind('/')] + '/' + latest
+        print(f'latest url: {latest_url}')
+
+        r = requests.get(latest_url, auth=(self.credentials.user, self.credentials.token))
+        print(f'response status code: {r.status_code}')
+
+        # save the tar
+        with open('poppy-latest.tar.gz', 'wb') as f:
+            f.write(r.content)
+            for name in tarfile.open('poppy-latest.tar.gz', mode='r|gz').getnames():
+                print(name)
+        with tarfile.open('poppy-latest.tar.gz', mode='r|gz') as f:
+            f.extractall()
+            # list the files
+            for name in f.getnames():
+                print(name)
+        # copy poppy to usr/bin or C:\Program Files\poppy
+
+        if platform.system() == 'Windows':
+            print('not implemented for windows')
+        else:
+            os.chmod('poppy', 0o777)
+            shutil.copy('poppy', '/usr/bin/poppy')
+
+        os.remove('poppy-latest.tar.gz')
+        os.remove('poppy')
+        return r
+
 
     def exists(self, _name, _version, _arch, _dist):
         url = self.url.format(name=_name, version=_version, arch=_arch, dist=_dist)
@@ -57,6 +104,7 @@ class Cargo(object):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--push', action='store_true')
+    parser.add_argument('--install-latest', action='store_true')
     parser.add_argument('--file', type=str)
     parser.add_argument('--name', type=str)
     parser.add_argument('--arch', type=str)
@@ -66,8 +114,11 @@ def main():
 
 
     artifactory = Artifactory(Credentials())
-    cargo = Cargo('./Cargo.toml')
+    if args.install_latest:
+        artifactory.install_latest(args.arch)
+
     if args.push:
+        cargo = Cargo('./Cargo.toml')
         file = open(args.file, 'rb')
         version = cargo.version()
         print(f'cargo version: {version}')

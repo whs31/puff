f22007c0446e58a79ed1957df48b84f27c035406
whs31 <kerbalspacefruit@gmail.com>
2024-03-21 17:46:28 +0300

    Update manifest.rs

--- src/manifest/manifest.rs
+++ src/manifest/manifest.rs
@@ -21,4 +21,41 @@ pub struct ManifestSectionThis
   pub license: Option<String>
 }
 
-// tests here
+#[cfg(test)]
+mod tests
+{
+  use super::*;
+
+  #[test]
+  fn test_deserialize()  {
+    let string = r#"
+    [this]
+    name = "test"
+    version = "10.0.0"
+    description = "test"
+    authors = ["author1", "author2"]
+    license = "MIT"
+
+    [needs]
+    fmt = "10.0.0"
+    qwe = "^10.0.0"
+    asd = "1.20.1@shared"
+    zxc = "=99.9.9@static"
+    ltst = "latest@shared"
+    "#;
+
+    let m: Manifest = toml::from_str(string).unwrap();
+    assert_eq!(m.this.name, "test");
+    assert_eq!(m.this.version, "10.0.0".parse().unwrap());
+    assert_eq!(m.this.description, Some("test".to_string()));
+    assert_eq!(m.this.authors, Some(vec![String::from("author1"), String::from("author2")]));
+    assert_eq!(m.this.license, Some("MIT".to_string()));
+    assert_eq!(m.needs, Some(HashMap::from([
+      ("fmt".to_string(), "10.0.0".parse().unwrap()),
+      ("qwe".to_string(), "^10.0.0".parse().unwrap()),
+      ("asd".to_string(), "1.20.1@shared".parse().unwrap()),
+      ("zxc".to_string(), "=99.9.9@static".parse().unwrap()),
+      ("ltst".to_string(), "latest@shared".parse().unwrap()),
+    ])));
+  }
+}

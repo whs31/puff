2b29235efd3cf5c6937db83e91c5a5e7b8eb5070
whs31 <kerbalspacefruit@gmail.com>
2024-03-21 15:50:17 +0300

    Manifest (basic + untested)

--- src/manifest/manifest.rs
+++ src/manifest/manifest.rs
@@ -1,29 +1,30 @@
-// use std::collections::HashMap;
-// use serde::{Deserialize, Serialize};
-//
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct Manifest
-// {
-//   pub this: ManifestSectionThis,
-//   pub needs: Option<HashMap<String, ManifestDependencyData>>,
-//   pub build: Option<HashMap<String, Version>>
-// }
-//
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct ManifestSectionThis
-// {
-//   pub name: String,
-//   pub version: Version,
-//   pub description: Option<String>,
-//   pub authors: Option<Vec<String>>,
-//   pub license: Option<String>
-// }
-//
-// #[derive(Debug, Serialize, Deserialize)]
-// pub struct ManifestDependencyData // todo: deserialize it from either string or struct
-// {
-//   pub version: Version,
-//   pub distribution: Option<Distribution>
-// }
-//
-// // tests here
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+use crate::types::{Distribution, VersionRange};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Manifest
+{
+  pub this: ManifestSectionThis,
+  pub needs: Option<HashMap<String, ManifestDependencyData>>,
+  pub build: Option<HashMap<String, VersionRange>>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ManifestSectionThis
+{
+  pub name: String,
+  pub version: VersionRange,
+  pub description: Option<String>,
+  pub authors: Option<Vec<String>>,
+  pub license: Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ManifestDependencyData // todo: deserialize it from either string or struct
+{
+  pub version: VersionRange,
+  pub distribution: Option<Distribution>
+}
+
+// tests here
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,3 +2,8 @@ mod version;
 mod distribution;
 mod arch;
 mod os;
+
+pub use version::*;
+pub use distribution::*;
+pub use arch::*;
+pub use os::*;

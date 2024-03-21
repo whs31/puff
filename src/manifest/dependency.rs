c29357a5e98cc3ff53faa8eaa948bd7461753227
whs31 <kerbalspacefruit@gmail.com>
2024-03-21 17:41:56 +0300

    Update dependency.rs

--- src/manifest/dependency.rs
+++ src/manifest/dependency.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use serde::{Deserialize, Serialize};
 use crate::types::{Distribution, VersionRange};
 
-#[derive(Debug, Clone, Copy, Hash, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Hash)]
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub struct ManifestDependencyData // todo: deserialize it from either string or struct
 {
@@ -34,6 +34,19 @@ impl FromStr for ManifestDependencyData {
   }
 }
 
+impl Serialize for ManifestDependencyData {
+  fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+    self.to_string().serialize(serializer)
+  }
+}
+
+impl<'de> Deserialize<'de> for ManifestDependencyData {
+  fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    s.parse().map_err(serde::de::Error::custom)
+  }
+}
+
 #[cfg(test)]
 mod tests
 {
@@ -58,5 +71,34 @@ mod tests
 
     let d = ManifestDependencyData::from_str("=10.0.0@static").unwrap();
     assert_eq!(d, ManifestDependencyData { version: "=10.0.0".parse().unwrap(), distribution: Distribution::Static });
+  }
+
+  #[test]
+  fn test_serde_ser() {
+    let d = ManifestDependencyData { version: "10.0.0".parse().unwrap(), distribution: Distribution::Shared };
+    let s = serde_json::to_string(&d).unwrap();
+    assert_eq!(s, "\"^10.0.0@shared\"");
+
+    let d = ManifestDependencyData { version: "=10.0.0".parse().unwrap(), distribution: Distribution::Static };
+    let s = serde_json::to_string(&d).unwrap();
+    assert_eq!(s, "\"=10.0.0@static\"");
+  }
+
+  #[test]
+  fn test_serde_de() {
+    let d = ManifestDependencyData { version: "10.0.0".parse().unwrap(), distribution: Distribution::Shared };
+    let s = serde_json::to_string(&d).unwrap();
+    let d_: ManifestDependencyData = serde_json::from_str(&s).unwrap();
+    assert_eq!(d, d_);
+
+    let d = ManifestDependencyData { version: "=10.0.0".parse().unwrap(), distribution: Distribution::Static };
+    let s = serde_json::to_string(&d).unwrap();
+    let d_: ManifestDependencyData = serde_json::from_str(&s).unwrap();
+    assert_eq!(d, d_);
+
+    let d = ManifestDependencyData { version: "10.0.0".parse().unwrap(), distribution: Distribution::Shared };
+    let s = serde_json::to_string(&d).unwrap();
+    let d_: ManifestDependencyData = serde_json::from_str(&s).unwrap();
+    assert_eq!(d, d_);
   }
 }

f6da03a9031648f4aea4f646aa966803a6e20705
whs31 <75864292+whs31@users.noreply.github.com>
2024-04-04 14:57:33 +0300

    Fixed cargo install/cargo build via crate

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    if !std::path::Path::new(".git").exists() {
+        return
+    }
     build_data::set_GIT_BRANCH();
     build_data::set_GIT_COMMIT();
     build_data::set_GIT_DIRTY();
